4216f90c5ba955da0bfec38d14f869ca2d4a8482
Smokvin <alex.smokvin@gmail.com>
2023-05-30 18:36:20 +0300

    Initial commit

---
+++ src/test/java/pro/sky/streamapiandoptional/StreamApiAndOptionalApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.streamapiandoptional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamApiAndOptionalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
